54c94c9a5f2c81a28c9a662673fcfd75346b3d2e
MaryBa <mariba0213@gmail.com>
2024-04-03 11:02:36 +0300

    ready index,html

---
+++ main_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from main_app import views
+
+app_name = 'main_app'
+
+urlpatterns = [
+    path('',views.index, name='index'),  ## cилка на views
+    path('about/',views.about, name='about'),  ## cилка на views
+    path('contact/',views.contact, name='contact'),  ## cилка на views
+
+]
